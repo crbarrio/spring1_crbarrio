26063cd3d522e8882393094de138b5e4fe3fb485
Carlos Ramirez <crbarrio@hotmail.com>
2025-12-02 12:10:15 +0100

    Improved accesibility for tabs and collapsible elements. Tabs styles doesnt use classes but role atributes and aria states.

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,20 +1,135 @@
 "use strict"
 
-function showTab(event, tabName) {
-    let tabcontent = document.getElementsByClassName('tabcontent');
-    for (let i = 0; i < tabcontent.length; i++) {
-        tabcontent[i].style.display = 'none';
+
+class TabsAutomatic {
+  constructor(groupNode) {
+    this.tablistNode = groupNode;
+
+    this.tabs = [];
+
+    this.firstTab = null;
+    this.lastTab = null;
+
+    this.tabs = Array.from(this.tablistNode.querySelectorAll('[role=tab]'));
+    this.tabpanels = [];
+
+    for (var i = 0; i < this.tabs.length; i += 1) {
+      var tab = this.tabs[i];
+      var tabpanel = document.getElementById(tab.getAttribute('aria-controls'));
+
+      tab.tabIndex = -1;
+      tab.setAttribute('aria-selected', 'false');
+      this.tabpanels.push(tabpanel);
+
+      tab.addEventListener('keydown', this.onKeydown.bind(this));
+      tab.addEventListener('click', this.onClick.bind(this));
+
+      if (!this.firstTab) {
+        this.firstTab = tab;
+      }
+      this.lastTab = tab;
     }
 
-    let tab_btn = document.getElementsByClassName('tab-btn');
-    for (let i = 0; i < tab_btn.length; i++) {
-       tab_btn[i].className = tab_btn[i].className.replace("active-tab", "")
+    this.setSelectedTab(this.firstTab, false);
+  }
+
+  setSelectedTab(currentTab, setFocus) {
+    if (typeof setFocus !== 'boolean') {
+      setFocus = true;
+    }
+    for (var i = 0; i < this.tabs.length; i += 1) {
+      var tab = this.tabs[i];
+      if (currentTab === tab) {
+        tab.setAttribute('aria-selected', 'true');
+        tab.removeAttribute('tabindex');
+        this.tabpanels[i].classList.remove('is-hidden');
+        if (setFocus) {
+          tab.focus();
+        }
+      } else {
+        tab.setAttribute('aria-selected', 'false');
+        tab.tabIndex = -1;
+        this.tabpanels[i].classList.add('is-hidden');
+      }
+    }
+  }
+
+  setSelectedToPreviousTab(currentTab) {
+    var index;
+
+    if (currentTab === this.firstTab) {
+      this.setSelectedTab(this.lastTab);
+    } else {
+      index = this.tabs.indexOf(currentTab);
+      this.setSelectedTab(this.tabs[index - 1]);
+    }
+  }
+
+  setSelectedToNextTab(currentTab) {
+    var index;
+
+    if (currentTab === this.lastTab) {
+      this.setSelectedTab(this.firstTab);
+    } else {
+      index = this.tabs.indexOf(currentTab);
+      this.setSelectedTab(this.tabs[index + 1]);
+    }
+  }
+
+  /* EVENT HANDLERS */
+
+  onKeydown(event) {
+    var tgt = event.currentTarget,
+      flag = false;
+
+    switch (event.key) {
+      case 'ArrowLeft':
+        this.setSelectedToPreviousTab(tgt);
+        flag = true;
+        break;
+
+      case 'ArrowRight':
+        this.setSelectedToNextTab(tgt);
+        flag = true;
+        break;
+
+      case 'Home':
+        this.setSelectedTab(this.firstTab);
+        flag = true;
+        break;
+
+      case 'End':
+        this.setSelectedTab(this.lastTab);
+        flag = true;
+        break;
+
+      default:
+        break;
     }
 
-    document.getElementById(tabName).style.display = 'flex';
-    event.currentTarget.className += ' active-tab'
+    if (flag) {
+      event.stopPropagation();
+      event.preventDefault();
+    }
+  }
 
+  onClick(event) {
+    this.setSelectedTab(event.currentTarget);
+  }
 }
+
+// Initialize tablist
+
+window.addEventListener('load', function () {
+  var tablists = document.querySelectorAll('[role=tablist]');
+  for (var i = 0; i < tablists.length; i++) {
+    new TabsAutomatic(tablists[i]);
+  }
+});
+
+
+
+
 
 const collapsibleElements = document.getElementsByClassName('collapse-btn')
 
@@ -22,6 +137,9 @@ for (let i = 0; i < collapsibleElements.length; i++) {
     collapsibleElements[i].addEventListener('click', function () {
         
         this.getElementsByTagName('svg')[0].classList.toggle('active-collapsible');
+
+        const isExpanded = this.getAttribute('aria-expanded') === 'true';
+        this.setAttribute('aria-expanded', !isExpanded);
 
         let content = this.nextElementSibling;
 
