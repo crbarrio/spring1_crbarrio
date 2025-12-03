ece5918d3b6deb5ad66d730157a6191c0f190191
Carlos Ramirez <crbarrio@hotmail.com>
2025-12-03 09:54:04 +0100

    Added classes and js controls for navbar functionality

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,180 +1,210 @@
 "use strict"
 
+const openMenu = document.getElementById('open-menu');
+const closeMenu = document.getElementById('close-menu');
+const navLinks = document.getElementById('nav-links');
+const navbar = document.getElementById('nav-bar');
 
-class TabsAutomatic {
-  constructor(groupNode) {
-    this.tablistNode = groupNode;
+openMenu.addEventListener('click', function () {
+    navLinks.classList.remove('hidden');
+    navLinks.classList.add('flex');
 
-    this.tabs = [];
+    openMenu.classList.add('hidden');
+    closeMenu.classList.remove('hidden');
+    navbar.classList.add('bg-slate-600/90');
+    navbar.classList.add('absolute');
 
-    this.firstTab = null;
-    this.lastTab = null;
+});
 
-    this.tabs = Array.from(this.tablistNode.querySelectorAll('[role=tab]'));
-    this.tabpanels = [];
+closeMenu.addEventListener('click', function () {
+    navLinks.classList.add('hidden');
+    navLinks.classList.remove('flex');
 
-    for (var i = 0; i < this.tabs.length; i += 1) {
-      var tab = this.tabs[i];
-      var tabpanel = document.getElementById(tab.getAttribute('aria-controls'));
+    closeMenu.classList.add('hidden');
+    openMenu.classList.remove('hidden');
+    navbar.classList.remove('bg-slate-600/90');
+    navbar.classList.remove('absolute');
 
-      tab.tabIndex = -1;
-      tab.setAttribute('aria-selected', 'false');
-      this.tabpanels.push(tabpanel);
-
-      tab.addEventListener('keydown', this.onKeydown.bind(this));
-      tab.addEventListener('click', this.onClick.bind(this));
-
-      if (!this.firstTab) {
-        this.firstTab = tab;
-      }
-      this.lastTab = tab;
-    }
-
-    this.setSelectedTab(this.firstTab, false);
-  }
-
-  setSelectedTab(currentTab, setFocus) {
-    if (typeof setFocus !== 'boolean') {
-      setFocus = true;
-    }
-    for (var i = 0; i < this.tabs.length; i += 1) {
-      var tab = this.tabs[i];
-      if (currentTab === tab) {
-        tab.setAttribute('aria-selected', 'true');
-        tab.removeAttribute('tabindex');
-        this.tabpanels[i].classList.remove('is-hidden');
-        if (setFocus) {
-          tab.focus();
-        }
-      } else {
-        tab.setAttribute('aria-selected', 'false');
-        tab.tabIndex = -1;
-        this.tabpanels[i].classList.add('is-hidden');
-      }
-    }
-  }
-
-  setSelectedToPreviousTab(currentTab) {
-    var index;
-
-    if (currentTab === this.firstTab) {
-      this.setSelectedTab(this.lastTab);
-    } else {
-      index = this.tabs.indexOf(currentTab);
-      this.setSelectedTab(this.tabs[index - 1]);
-    }
-  }
-
-  setSelectedToNextTab(currentTab) {
-    var index;
-
-    if (currentTab === this.lastTab) {
-      this.setSelectedTab(this.firstTab);
-    } else {
-      index = this.tabs.indexOf(currentTab);
-      this.setSelectedTab(this.tabs[index + 1]);
-    }
-  }
-
-  /* EVENT HANDLERS */
-
-  onKeydown(event) {
-    var tgt = event.currentTarget,
-      flag = false;
-
-    switch (event.key) {
-      case 'ArrowLeft':
-        this.setSelectedToPreviousTab(tgt);
-        flag = true;
-        break;
-
-      case 'ArrowRight':
-        this.setSelectedToNextTab(tgt);
-        flag = true;
-        break;
-
-      case 'Home':
-        this.setSelectedTab(this.firstTab);
-        flag = true;
-        break;
-
-      case 'End':
-        this.setSelectedTab(this.lastTab);
-        flag = true;
-        break;
-
-      default:
-        break;
-    }
-
-    if (flag) {
-      event.stopPropagation();
-      event.preventDefault();
-    }
-  }
-
-  onClick(event) {
-    this.setSelectedTab(event.currentTarget);
-  }
-}
-
-// Initialize tablist
-
-window.addEventListener('load', function () {
-  var tablists = document.querySelectorAll('[role=tablist]');
-  for (var i = 0; i < tablists.length; i++) {
-    new TabsAutomatic(tablists[i]);
-  }
 });
 
 
-const collapsibleElements = document.getElementsByClassName('collapse-btn')
 
-for (let i = 0; i < collapsibleElements.length; i++) {
-    collapsibleElements[i].addEventListener('click', function () {
+
+
+// class TabsAutomatic {
+//   constructor(groupNode) {
+//     this.tablistNode = groupNode;
+
+//     this.tabs = [];
+
+//     this.firstTab = null;
+//     this.lastTab = null;
+
+//     this.tabs = Array.from(this.tablistNode.querySelectorAll('[role=tab]'));
+//     this.tabpanels = [];
+
+//     for (var i = 0; i < this.tabs.length; i += 1) {
+//       var tab = this.tabs[i];
+//       var tabpanel = document.getElementById(tab.getAttribute('aria-controls'));
+
+//       tab.tabIndex = -1;
+//       tab.setAttribute('aria-selected', 'false');
+//       this.tabpanels.push(tabpanel);
+
+//       tab.addEventListener('keydown', this.onKeydown.bind(this));
+//       tab.addEventListener('click', this.onClick.bind(this));
+
+//       if (!this.firstTab) {
+//         this.firstTab = tab;
+//       }
+//       this.lastTab = tab;
+//     }
+
+//     this.setSelectedTab(this.firstTab, false);
+//   }
+
+//   setSelectedTab(currentTab, setFocus) {
+//     if (typeof setFocus !== 'boolean') {
+//       setFocus = true;
+//     }
+//     for (var i = 0; i < this.tabs.length; i += 1) {
+//       var tab = this.tabs[i];
+//       if (currentTab === tab) {
+//         tab.setAttribute('aria-selected', 'true');
+//         tab.removeAttribute('tabindex');
+//         this.tabpanels[i].classList.remove('is-hidden');
+//         if (setFocus) {
+//           tab.focus();
+//         }
+//       } else {
+//         tab.setAttribute('aria-selected', 'false');
+//         tab.tabIndex = -1;
+//         this.tabpanels[i].classList.add('is-hidden');
+//       }
+//     }
+//   }
+
+//   setSelectedToPreviousTab(currentTab) {
+//     var index;
+
+//     if (currentTab === this.firstTab) {
+//       this.setSelectedTab(this.lastTab);
+//     } else {
+//       index = this.tabs.indexOf(currentTab);
+//       this.setSelectedTab(this.tabs[index - 1]);
+//     }
+//   }
+
+//   setSelectedToNextTab(currentTab) {
+//     var index;
+
+//     if (currentTab === this.lastTab) {
+//       this.setSelectedTab(this.firstTab);
+//     } else {
+//       index = this.tabs.indexOf(currentTab);
+//       this.setSelectedTab(this.tabs[index + 1]);
+//     }
+//   }
+
+//   /* EVENT HANDLERS */
+
+//   onKeydown(event) {
+//     var tgt = event.currentTarget,
+//       flag = false;
+
+//     switch (event.key) {
+//       case 'ArrowLeft':
+//         this.setSelectedToPreviousTab(tgt);
+//         flag = true;
+//         break;
+
+//       case 'ArrowRight':
+//         this.setSelectedToNextTab(tgt);
+//         flag = true;
+//         break;
+
+//       case 'Home':
+//         this.setSelectedTab(this.firstTab);
+//         flag = true;
+//         break;
+
+//       case 'End':
+//         this.setSelectedTab(this.lastTab);
+//         flag = true;
+//         break;
+
+//       default:
+//         break;
+//     }
+
+//     if (flag) {
+//       event.stopPropagation();
+//       event.preventDefault();
+//     }
+//   }
+
+//   onClick(event) {
+//     this.setSelectedTab(event.currentTarget);
+//   }
+// }
+
+// // Initialize tablist
+
+// window.addEventListener('load', function () {
+//   var tablists = document.querySelectorAll('[role=tablist]');
+//   for (var i = 0; i < tablists.length; i++) {
+//     new TabsAutomatic(tablists[i]);
+//   }
+// });
+
+
+// const collapsibleElements = document.getElementsByClassName('collapse-btn')
+
+// for (let i = 0; i < collapsibleElements.length; i++) {
+//     collapsibleElements[i].addEventListener('click', function () {
         
-        this.getElementsByTagName('svg')[0].classList.toggle('active-collapsible');
+//         this.getElementsByTagName('svg')[0].classList.toggle('active-collapsible');
 
-        const isExpanded = this.getAttribute('aria-expanded') === 'true';
-        this.setAttribute('aria-expanded', !isExpanded);
+//         const isExpanded = this.getAttribute('aria-expanded') === 'true';
+//         this.setAttribute('aria-expanded', !isExpanded);
 
-        let content = this.nextElementSibling;
+//         let content = this.nextElementSibling;
 
-        if (content.style.maxHeight){
-            content.style.maxHeight = null;
-        } else {
-            content.style.maxHeight = content.scrollHeight + "px";
-        }
-    })
-}
+//         if (content.style.maxHeight){
+//             content.style.maxHeight = null;
+//         } else {
+//             content.style.maxHeight = content.scrollHeight + "px";
+//         }
+//     })
+// }
 
 
 
-const formContact = document.getElementById('formContact');
+// const formContact = document.getElementById('formContact');
 
-formContact.addEventListener('submit', function(event) {
-    event.preventDefault();
-    const emailInput = this.querySelector('input[type="text"]');
-    const email = emailInput.value.trim();
-    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+// formContact.addEventListener('submit', function(event) {
+//     event.preventDefault();
+//     const emailInput = this.querySelector('input[type="text"]');
+//     const email = emailInput.value.trim();
+//     const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
     
-    const errorMessage = document.getElementById('errorMessage');
+//     const errorMessage = document.getElementById('errorMessage');
 
-    errorMessage.style.display = 'none';
-    emailInput.classList.remove('input-error');
+//     errorMessage.style.display = 'none';
+//     emailInput.classList.remove('input-error');
 
-    if (!emailRegex.test(email)) {
-        errorMessage.style.display = 'block';
-        emailInput.classList.add('input-error');
-    }
-});
+//     if (!emailRegex.test(email)) {
+//         errorMessage.style.display = 'block';
+//         emailInput.classList.add('input-error');
+//     }
+// });
 
-const mobileMenuElements = document.getElementsByClassName('mobile-menu')
+// const mobileMenuElements = document.getElementsByClassName('mobile-menu')
 
-for (let i = 0; i < mobileMenuElements.length; i++) {
-    mobileMenuElements[i].addEventListener('click', function () {
+// for (let i = 0; i < mobileMenuElements.length; i++) {
+//     mobileMenuElements[i].addEventListener('click', function () {
         
-        document.getElementById('navbar').classList.toggle('mobile');
-    })
-}
+//         document.getElementById('navbar').classList.toggle('mobile');
+//     })
+// }
 
