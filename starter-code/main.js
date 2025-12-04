6b2b054e1616c28df2b8fce915f330f6ea5dbce9
Carlos Ramirez <crbarrio@hotmail.com>
2025-12-04 16:14:17 +0100

    Added tailwind css generator, Added js functionality for tabs and accordion

--- starter-code/main.js
+++ starter-code/main.js
@@ -4,6 +4,7 @@ const openMenu = document.getElementById('open-menu');
 const closeMenu = document.getElementById('close-menu');
 const navLinks = document.getElementById('nav-links');
 const navbar = document.getElementById('nav-bar');
+const logoBookmark = document.getElementById('logo-bookmark');
 
 openMenu.addEventListener('click', function () {
     navLinks.classList.remove('hidden');
@@ -13,6 +14,7 @@ openMenu.addEventListener('click', function () {
     closeMenu.classList.remove('hidden');
     navbar.classList.add('bg-slate-600/90');
     navbar.classList.add('absolute');
+    logoBookmark.classList.add('fill-white');
 
 });
 
@@ -24,6 +26,7 @@ closeMenu.addEventListener('click', function () {
     openMenu.classList.remove('hidden');
     navbar.classList.remove('bg-slate-600/90');
     navbar.classList.remove('absolute');
+    logoBookmark.classList.remove('fill-white');
 
 });
 
@@ -31,180 +34,191 @@ closeMenu.addEventListener('click', function () {
 
 
 
-// class TabsAutomatic {
-//   constructor(groupNode) {
-//     this.tablistNode = groupNode;
+class TabsAutomatic {
+  constructor(groupNode) {
+    this.tablistNode = groupNode;
 
-//     this.tabs = [];
+    this.tabs = [];
 
-//     this.firstTab = null;
-//     this.lastTab = null;
+    this.firstTab = null;
+    this.lastTab = null;
 
-//     this.tabs = Array.from(this.tablistNode.querySelectorAll('[role=tab]'));
-//     this.tabpanels = [];
+    this.tabs = Array.from(this.tablistNode.querySelectorAll('[role=tab]'));
+    this.tabpanels = [];
 
-//     for (var i = 0; i < this.tabs.length; i += 1) {
-//       var tab = this.tabs[i];
-//       var tabpanel = document.getElementById(tab.getAttribute('aria-controls'));
+    for (var i = 0; i < this.tabs.length; i += 1) {
+      var tab = this.tabs[i];
+      var tabpanel = document.getElementById(tab.getAttribute('aria-controls'));
 
-//       tab.tabIndex = -1;
-//       tab.setAttribute('aria-selected', 'false');
-//       this.tabpanels.push(tabpanel);
+      tab.tabIndex = -1;
+      tab.setAttribute('aria-selected', 'false');
+      this.tabpanels.push(tabpanel);
 
-//       tab.addEventListener('keydown', this.onKeydown.bind(this));
-//       tab.addEventListener('click', this.onClick.bind(this));
+      tab.addEventListener('keydown', this.onKeydown.bind(this));
+      tab.addEventListener('click', this.onClick.bind(this));
 
-//       if (!this.firstTab) {
-//         this.firstTab = tab;
-//       }
-//       this.lastTab = tab;
-//     }
+      if (!this.firstTab) {
+        this.firstTab = tab;
+      }
+      this.lastTab = tab;
+    }
 
-//     this.setSelectedTab(this.firstTab, false);
-//   }
+    this.setSelectedTab(this.firstTab, false);
+  }
 
-//   setSelectedTab(currentTab, setFocus) {
-//     if (typeof setFocus !== 'boolean') {
-//       setFocus = true;
-//     }
-//     for (var i = 0; i < this.tabs.length; i += 1) {
-//       var tab = this.tabs[i];
-//       if (currentTab === tab) {
-//         tab.setAttribute('aria-selected', 'true');
-//         tab.removeAttribute('tabindex');
-//         this.tabpanels[i].classList.remove('is-hidden');
-//         if (setFocus) {
-//           tab.focus();
-//         }
-//       } else {
-//         tab.setAttribute('aria-selected', 'false');
-//         tab.tabIndex = -1;
-//         this.tabpanels[i].classList.add('is-hidden');
-//       }
-//     }
-//   }
-
-//   setSelectedToPreviousTab(currentTab) {
-//     var index;
-
-//     if (currentTab === this.firstTab) {
-//       this.setSelectedTab(this.lastTab);
-//     } else {
-//       index = this.tabs.indexOf(currentTab);
-//       this.setSelectedTab(this.tabs[index - 1]);
-//     }
-//   }
-
-//   setSelectedToNextTab(currentTab) {
-//     var index;
-
-//     if (currentTab === this.lastTab) {
-//       this.setSelectedTab(this.firstTab);
-//     } else {
-//       index = this.tabs.indexOf(currentTab);
-//       this.setSelectedTab(this.tabs[index + 1]);
-//     }
-//   }
-
-//   /* EVENT HANDLERS */
-
-//   onKeydown(event) {
-//     var tgt = event.currentTarget,
-//       flag = false;
-
-//     switch (event.key) {
-//       case 'ArrowLeft':
-//         this.setSelectedToPreviousTab(tgt);
-//         flag = true;
-//         break;
-
-//       case 'ArrowRight':
-//         this.setSelectedToNextTab(tgt);
-//         flag = true;
-//         break;
-
-//       case 'Home':
-//         this.setSelectedTab(this.firstTab);
-//         flag = true;
-//         break;
-
-//       case 'End':
-//         this.setSelectedTab(this.lastTab);
-//         flag = true;
-//         break;
-
-//       default:
-//         break;
-//     }
-
-//     if (flag) {
-//       event.stopPropagation();
-//       event.preventDefault();
-//     }
-//   }
-
-//   onClick(event) {
-//     this.setSelectedTab(event.currentTarget);
-//   }
-// }
-
-// // Initialize tablist
-
-// window.addEventListener('load', function () {
-//   var tablists = document.querySelectorAll('[role=tablist]');
-//   for (var i = 0; i < tablists.length; i++) {
-//     new TabsAutomatic(tablists[i]);
-//   }
-// });
+  setSelectedTab(currentTab, setFocus) {
+    if (typeof setFocus !== 'boolean') {
+      setFocus = true;
+    }
+    for (var i = 0; i < this.tabs.length; i += 1) {
+      var tab = this.tabs[i];
+      if (currentTab === tab) {
+        tab.setAttribute('aria-selected', 'true');
+        tab.removeAttribute('tabindex');
+        tab.classList.remove('sm:border-transparent');
+        tab.classList.add('border-b-2', 'border-red-500', 'text-gray-900');
+        this.tabpanels[i].classList.add('flex');
+        this.tabpanels[i].classList.remove('hidden');
 
 
-// const collapsibleElements = document.getElementsByClassName('collapse-btn')
+        if (setFocus) {
+          tab.focus();
+        }
+      } else {
+        tab.setAttribute('aria-selected', 'false');
+        tab.tabIndex = -1;
+        tab.classList.remove('border-b-2', 'border-red-500', 'text-gray-900');
+        tab.classList.add('sm:border-transparent');
+        this.tabpanels[i].classList.add('hidden');
+        this.tabpanels[i].classList.remove('flex');
+      }
+    }
+  }
 
-// for (let i = 0; i < collapsibleElements.length; i++) {
-//     collapsibleElements[i].addEventListener('click', function () {
-        
-//         this.getElementsByTagName('svg')[0].classList.toggle('active-collapsible');
+  setSelectedToPreviousTab(currentTab) {
+    var index;
 
-//         const isExpanded = this.getAttribute('aria-expanded') === 'true';
-//         this.setAttribute('aria-expanded', !isExpanded);
+    if (currentTab === this.firstTab) {
+      this.setSelectedTab(this.lastTab);
+    } else {
+      index = this.tabs.indexOf(currentTab);
+      this.setSelectedTab(this.tabs[index - 1]);
+    }
+  }
 
-//         let content = this.nextElementSibling;
+  setSelectedToNextTab(currentTab) {
+    var index;
 
-//         if (content.style.maxHeight){
-//             content.style.maxHeight = null;
-//         } else {
-//             content.style.maxHeight = content.scrollHeight + "px";
-//         }
-//     })
-// }
+    if (currentTab === this.lastTab) {
+      this.setSelectedTab(this.firstTab);
+    } else {
+      index = this.tabs.indexOf(currentTab);
+      this.setSelectedTab(this.tabs[index + 1]);
+    }
+  }
+
+  /* EVENT HANDLERS */
+
+  onKeydown(event) {
+    var tgt = event.currentTarget,
+      flag = false;
+
+    switch (event.key) {
+      case 'ArrowLeft':
+        this.setSelectedToPreviousTab(tgt);
+        flag = true;
+        break;
+
+      case 'ArrowRight':
+        this.setSelectedToNextTab(tgt);
+        flag = true;
+        break;
+
+      case 'Home':
+        this.setSelectedTab(this.firstTab);
+        flag = true;
+        break;
+
+      case 'End':
+        this.setSelectedTab(this.lastTab);
+        flag = true;
+        break;
+
+      default:
+        break;
+    }
+
+    if (flag) {
+      event.stopPropagation();
+      event.preventDefault();
+    }
+  }
+
+  onClick(event) {
+    this.setSelectedTab(event.currentTarget);
+  }
+}
+
+// Initialize tablist
+
+window.addEventListener('load', function () {
+  var tablists = document.querySelectorAll('[role=tablist]');
+  for (var i = 0; i < tablists.length; i++) {
+    new TabsAutomatic(tablists[i]);
+  }
+});
+
+
+const collapsibleElements = document.getElementsByClassName('collapse-btn')
+
+for (let i = 0; i < collapsibleElements.length; i++) {
+    collapsibleElements[i].addEventListener('click', function () {
+
+        this.getElementsByTagName('svg')[0].getElementsByTagName('path')[0].classList.toggle('stroke-red-500');
+        this.getElementsByTagName('svg')[0].classList.toggle('rotate-180');
+
+        const isExpanded = this.getAttribute('aria-expanded') === 'true';
+        this.setAttribute('aria-expanded', !isExpanded);
+
+        let content = this.nextElementSibling;
+
+        if (content.classList.contains('max-h-0')) {
+            content.classList.remove('max-h-0', 'hidden');
+            content.classList.add('max-h-screen');
+        } else {
+            content.classList.add('max-h-0', 'hidden');
+            content.classList.remove('max-h-screen');
+        }
+    })
+}
 
 
 
-// const formContact = document.getElementById('formContact');
+const formContact = document.getElementById('formContact');
 
-// formContact.addEventListener('submit', function(event) {
-//     event.preventDefault();
-//     const emailInput = this.querySelector('input[type="text"]');
-//     const email = emailInput.value.trim();
-//     const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+formContact.addEventListener('submit', function(event) {
+    event.preventDefault();
+    const emailInput = this.querySelector('input[type="text"]');
+    const email = emailInput.value.trim();
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
     
-//     const errorMessage = document.getElementById('errorMessage');
+    const errorMessage = document.getElementById('errorMessage');
 
-//     errorMessage.style.display = 'none';
-//     emailInput.classList.remove('input-error');
+    errorMessage.style.display = 'none';
+    emailInput.classList.remove('input-error');
 
-//     if (!emailRegex.test(email)) {
-//         errorMessage.style.display = 'block';
-//         emailInput.classList.add('input-error');
-//     }
-// });
+    if (!emailRegex.test(email)) {
+        errorMessage.style.display = 'block';
+        emailInput.classList.add('input-error');
+    }
+});
 
-// const mobileMenuElements = document.getElementsByClassName('mobile-menu')
+const mobileMenuElements = document.getElementsByClassName('mobile-menu')
 
-// for (let i = 0; i < mobileMenuElements.length; i++) {
-//     mobileMenuElements[i].addEventListener('click', function () {
+for (let i = 0; i < mobileMenuElements.length; i++) {
+    mobileMenuElements[i].addEventListener('click', function () {
         
-//         document.getElementById('navbar').classList.toggle('mobile');
-//     })
-// }
+        document.getElementById('navbar').classList.toggle('mobile');
+    })
+}
 
